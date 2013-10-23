26fe71e63c022d0846a1d0e32313896cc6bcba0f
Martin Rosén-Lidholm <mrol@telenor.dk>
2013-10-23 10:45:41 +0200

    add click event handler

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-﻿function addButton() {
-    $("#submit-button").parent().append("<button type='button' class='button primary'>Create AgileZen story</button>");
+﻿function createAgileZenStory() {
+    alert("hello zd!");
+}
+
+function addButton() {
+    $("#submit-button").parent().append("<button id='create-agilezen-story-button' type='button' class='button primary'>Create AgileZen story</button>");
+
+    $("#create-agilezen-story-button").click(createAgileZenStory);
 }
 
 addButton();
