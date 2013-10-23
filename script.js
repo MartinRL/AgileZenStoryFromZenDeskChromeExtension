eb0dfa726b32372d24c8e4c0b12bde096d921283
Martin Rosén-Lidholm <mrol@telenor.dk>
2013-10-23 12:14:21 +0200

    add button

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿function createAgileZenStory() {
+    alert("hello zd!");
+}
+
+function addButton() {
+    $("#submit-button").parent().append("<button id='create-agilezen-story-button' type='button' class='button primary'>Create AgileZen story</button>");
+
+    $("#create-agilezen-story-button").click(createAgileZenStory);
+}
+
+addButton();
