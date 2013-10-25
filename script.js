e1f26b8a49459f58a8c778ec7f2b381cd013aaf6
Martin Rosén-Lidholm <mrol@telenor.dk>
2013-10-25 13:43:30 +0200

    setAgileZenApiUrl

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 ﻿var options;
 var ALL = null;
+var agileZenApiUrl;
 
 function setOptions(continuation) {
     chrome.storage.sync.get(ALL,
@@ -9,17 +10,22 @@ function setOptions(continuation) {
         });
 };
 
-function setAjaxDefaults(continuation) {
+function setAjaxDefaults() {
     $.ajaxSetup({
         xhrFields: { withCredentials: true },
-        beforeSend: function (xhr) { xhr.setRequestHeader("X-Zen-ApiKey", options.api_key); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Zen-ApiKey", options.api_key);
+        },
     });
-    continuation();
 };
 
 function createAgileZenStory() {
-    alert("api key: " + options.api_key + ", board#: " + options.board_number);
+    alert("api key: " + options.api_key + ", board#: " + options.board_number + ", agileZenApiUrl: " + agileZenApiUrl);
 }
+
+function setAgileZenApiUrl() {
+    agileZenApiUrl = "https://agilezen.com/api/v1/projects/" + options.board_number + "/stories";
+};
 
 function addButton() {
     $("#submit-button").parent().append("<button id='create-agilezen-story-button' type='button' class='button primary'>Create AgileZen story</button>");
@@ -27,7 +33,13 @@ function addButton() {
     $("#create-agilezen-story-button").click(createAgileZenStory);
 }
 
-setOptions(setAjaxDefaults(addButton));
+setOptions(function () {
+    addButton();
+
+    setAjaxDefaults();
+
+    setAgileZenApiUrl();
+});
 
 chrome.storage.onChanged.addListener(function () {
     setOptions(function () {
