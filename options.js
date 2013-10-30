39ec08f8b832694490d1d1954ac22140d62d2b82
Martin Rosén-Lidholm <mrol@telenor.dk>
2013-10-30 12:55:02 +0100

    add user name to options

--- options.js
+++ options.js
@@ -2,7 +2,8 @@
     var saveOptions = function () {
         chrome.storage.sync.set({
             "api_key": $("#api_key").val(),
-            "board_number": $("#board_number").val()
+            "board_number": $("#board_number").val(),
+            "user_name": $("#user_name").val()
         });
 
         var status = $("#status");
@@ -17,6 +18,7 @@
             function (options) {
                 $("#api_key").val(options.api_key);
                 $("#board_number").val(options.board_number);
+                $("#user_name").val(options.user_name);
             });
     };
 
